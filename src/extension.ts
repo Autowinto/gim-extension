ad71709711b60cf2e73d00d27ea00076e50d3808
Magnus Christian Larsen <happymansi@hotmail.com>
2025-09-17 10:27:08 +0200

    feat: more structure

--- src/extension.ts
+++ src/extension.ts
@@ -1,28 +1,55 @@
 import * as vscode from 'vscode'
 
 export function activate(context: vscode.ExtensionContext) {
-  console.log('Congratulations, your extension "gim" is now active!')
+  console.log('Activating GIM')
 
   context.subscriptions.push(
-    vscode.commands.registerCommand('gim.selection.docstring', () => {
-      vscode.window.showInformationMessage('GIM: TODO IMPLEMENT DOCSTRING SELECTION!')
-    }),
+    vscode.commands.registerCommand(
+      'gim.selection.docstring',
+      docstringFromSelection,
+    ),
   )
   context.subscriptions.push(
-    vscode.commands.registerCommand('gim.selection.analyze', () => {
-      vscode.window.showInformationMessage('GIM: TODO IMPLEMENT ANALYZE SELECTION!')
-    }),
+    vscode.commands.registerCommand(
+      'gim.selection.analyze',
+      analyzeSelection,
+    ),
   )
   context.subscriptions.push(
-    vscode.commands.registerCommand('gim.file.analyze', () => {
-      vscode.window.showInformationMessage('GIM: TODO IMPLEMENT ANALYZE FILE!')
-    }),
+    vscode.commands.registerCommand(
+      'gim.file.analyze',
+      analyzeFile,
+    ),
   )
   context.subscriptions.push(
-    vscode.commands.registerCommand('gim.selection.explain', () => {
-      vscode.window.showInformationMessage('GIM: TODO IMPLEMENT EXPLAIN SELECTION!')
-    }),
+    vscode.commands.registerCommand(
+      'gim.selection.explain',
+      explainFromSelection,
+    ),
   )
 }
 
-export function deactivate() {}
+function docstringFromSelection() {
+  vscode.window.showInformationMessage('GIM: TODO IMPLEMENT DOCSTRING SELECTION!')
+}
+
+function analyzeSelection() {
+  vscode.window.showInformationMessage('GIM: TODO IMPLEMENT SELECTION ANALYSIS!')
+}
+
+function explainFromSelection() {
+  vscode.window.showInformationMessage('GIM: TODO IMPLEMENT EXPLAIN SELECTION!')
+}
+
+function analyzeFile() {
+  const editor = vscode.window.activeTextEditor
+  if (editor) {
+    vscode.window.showInformationMessage(`GIM: Current file is !${editor.document}`)
+    return
+  }
+
+  vscode.window.showErrorMessage('GIM: No file open')
+}
+
+export function deactivate() {
+}
