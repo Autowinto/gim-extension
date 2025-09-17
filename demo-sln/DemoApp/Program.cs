e6004ef78de6dacef3ae99328fb1e1de440a794b
Rasmus Wittrock <rasmus.wittrock@gmail.com>
2025-09-17 11:10:28 +0200

    initial analyzer + gitignore update

---
+++ demo-sln/DemoApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using DemoLib;
+
+class Program
+{
+    static void Main()
+    {
+        var calc = new Calculator();
+
+        int a = 10, b = 5;
+        Console.WriteLine($"Add: {calc.Add(a, b)}");
+        Console.WriteLine($"Subtract: {calc.Subtract(a, b)}");
+        Console.WriteLine($"Multiply: {calc.Multiply(a, b)}");
+        Console.WriteLine($"Divide: {calc.Divide(a, b)}");
+
+        Console.WriteLine($"Square: {MathUtils.Square(a)}");
+        Console.WriteLine($"Sum: {MathUtils.Sum(1, 2, 3, 4, 5)}");
+    }
+}
---
+++ demo-sln/DemoLib/Class1.cs
@@ -0,0 +1,29 @@
+﻿namespace DemoLib;
+
+public class Calculator
+{
+    public int Add(int a, int b) => a + b;
+    public int Subtract(int a, int b) => a - b;
+    public int Multiply(int a, int b) => a * b;
+
+    public double Divide(int a, int b)
+    {
+        if (b == 0)
+            throw new DivideByZeroException();
+        int x = Add(1, 1);
+        return (double)a / b;
+    }
+}
+
+public static class MathUtils
+{
+    public static int Square(int x) => x * x;
+
+    public static int Sum(params int[] numbers)
+    {
+        int total = 0;
+        foreach (var n in numbers)
+            total += n;
+        return total;
+    }
+}
