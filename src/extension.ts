287095960178e5f0df62d0c2aabb00e830bd60d8
Magnus Christian Larsen <happymansi@hotmail.com>
2025-09-17 10:18:10 +0200

    feat: added outline of commands

--- src/extension.ts
+++ src/extension.ts
@@ -3,11 +3,26 @@ import * as vscode from 'vscode'
 export function activate(context: vscode.ExtensionContext) {
   console.log('Congratulations, your extension "gim" is now active!')
 
-  const disposable = vscode.commands.registerCommand('gim.run', () => {
-    vscode.window.showInformationMessage('Hello World from gim!')
-  })
-
-  context.subscriptions.push(disposable)
+  context.subscriptions.push(
+    vscode.commands.registerCommand('gim.selection.docstring', () => {
+      vscode.window.showInformationMessage('GIM: TODO IMPLEMENT DOCSTRING SELECTION!')
+    }),
+  )
+  context.subscriptions.push(
+    vscode.commands.registerCommand('gim.selection.analyze', () => {
+      vscode.window.showInformationMessage('GIM: TODO IMPLEMENT ANALYZE SELECTION!')
+    }),
+  )
+  context.subscriptions.push(
+    vscode.commands.registerCommand('gim.file.analyze', () => {
+      vscode.window.showInformationMessage('GIM: TODO IMPLEMENT ANALYZE FILE!')
+    }),
+  )
+  context.subscriptions.push(
+    vscode.commands.registerCommand('gim.selection.explain', () => {
+      vscode.window.showInformationMessage('GIM: TODO IMPLEMENT EXPLAIN SELECTION!')
+    }),
+  )
 }
 
 export function deactivate() {}
